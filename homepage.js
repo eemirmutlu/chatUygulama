46d23a4b6aea9df5cba798cd8bc4650075fe5091
eemirmutlu <147301082+eemirmutlu@users.noreply.github.com>
2023-12-25 03:35:58 +0300

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,3 +2,21 @@
 document.getElementById('username').textContent = localStorage.getItem('username');
 document.getElementById('email').textContent = localStorage.getItem('email');
 document.getElementById('bio').textContent = localStorage.getItem('bio');
+
+document.addEventListener("DOMContentLoaded", function() {
+    const isOnline = Math.random() < 0.5;
+
+    const onlineIndicator = document.getElementById('onlineIndicator')
+
+    if (isOnline) {
+        onlineIndicator.style.backgroundColor= 'green'
+        onlineText.textContent = 'Çevrimiçi'
+    } else {
+        onlineIndicator.style.backgroundColor= 'red'
+        onlineText.textContent = 'Çervimdışı'
+    }
+})
+
+function logOut(){
+    alert('Giriş sayfasına dönüyorsunuz.')
+}
